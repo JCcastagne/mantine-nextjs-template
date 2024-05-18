552fadade5518a5d5743aa049bd5b7a56aaa9e1d
JC <jc.castagne96@gmail.com>
2024-05-18 14:33:17 -0400

    Add custom color(s), declare default color.

--- theme.ts
+++ theme.ts
@@ -1,7 +1,25 @@
-"use client";
+'use client'
 
-import { createTheme } from "@mantine/core";
+import { createTheme, MantineColorsTuple } from '@mantine/core'
+
+const purple: MantineColorsTuple = [
+  // "Purple": https://mantine.dev/colors-generator/?color=4F23C0
+  '#f3edff',
+  '#e0d7fa',
+  '#beabf0',
+  '#9a7ce6',
+  '#7c56de',
+  '#683dd9',
+  '#5f2fd8',
+  '#4f23c0',
+  '#451eac',
+  '#3a1899'
+]
 
 export const theme = createTheme({
   /* Put your mantine theme override here */
-});
+  colors: {
+    purple
+  },
+  primaryColor: 'purple'
+})
