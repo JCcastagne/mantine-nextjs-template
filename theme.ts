082f5c0f87659890dec8482d3784ef3d0bac33e2
JC <jc.castagne96@gmail.com>
2024-05-18 22:33:07 -0400

    Remove custom color(s).

--- theme.ts
+++ theme.ts
@@ -1,27 +1,10 @@
 'use client'
 
-import { createTheme, MantineColorsTuple } from '@mantine/core'
-
-const purple: MantineColorsTuple = [
-  // "Purple": https://mantine.dev/colors-generator/?color=4F23C0
-  '#f3edff',
-  '#e0d7fa',
-  '#beabf0',
-  '#9a7ce6',
-  '#7c56de',
-  '#683dd9',
-  '#5f2fd8',
-  '#4f23c0',
-  '#451eac',
-  '#3a1899'
-]
+import { createTheme } from '@mantine/core'
 
 export const theme = createTheme({
   /* Put your mantine theme override here */
-  colors: {
-    purple
-  },
-  primaryColor: 'purple',
+  primaryColor: 'violet',
   fontFamily:
     'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji',
   headings: {
