e4df00869762720d35aa9a4aed51a45c88455b02
JC <jc.castagne96@gmail.com>
2024-05-20 22:21:02 -0400

    Add root background color. Add header.

---
+++ app/AppContainer.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { Box, Container, useComputedColorScheme } from '@mantine/core'
+import { useColorScheme } from '@mantine/hooks'
+import Header from './Components/Header'
+
+export default function AppContainer ({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  const colorScheme = useComputedColorScheme()
+
+  return (
+    <Box
+      style={{
+        width: '100vw',
+        height: '100vh',
+        backgroundColor:
+          colorScheme === 'light'
+            ? 'var(--mantine-color-gray-1)'
+            : 'var(--mantine-color-gray-9)'
+      }}
+    >
+      <Container size='xl'>
+        <Header />
+        {children}
+      </Container>
+    </Box>
+  )
+}
