17904f66b5147d182c21ef9c2193e160d7d9438e
JC <jc.castagne96@gmail.com>
2024-08-05 19:25:22 -0400

    Extend shadows variable (add xxl).

--- theme.ts
+++ theme.ts
@@ -1,7 +1,5 @@
 'use client'
-
 import { createTheme } from '@mantine/core'
-
 export const theme = createTheme({
   /* Put your mantine theme override here */
   primaryColor: 'violet',
@@ -10,5 +8,11 @@ export const theme = createTheme({
   headings: {
     fontFamily:
       'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji'
+  },
+  components: {
+    //
+  },
+  shadows: {
+    xxl: '0 1px 3px rgba(0, 0, 0, 0.05), 0 44px 35px -7px rgba(0, 0, 0, 0.05), 0 22px 22px -7px rgba(0, 0, 0, 0.04)'
   }
 })
